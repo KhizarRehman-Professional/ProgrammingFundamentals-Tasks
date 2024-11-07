eceb4082c0b475ad1cce96cd3ce51e8088cb198b
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-07 16:43:06 +0500

    Create LabTask-Week3-Task1.cpp

---
+++ Week-3/LabTask-Week3-Task1.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file    LabTask-Week3-Task1.cpp
+ * @author  Khizar Rehman
+ * @brief   This program calculates the circumference of a circle.
+ * @details The program asks the user to enter the radius of the circle and then calculates the circumference using the formula 2 * pi * r.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14, radius, circumference;
+
+    // Ask the user to enter the radius of the circle
+    cout << "Enter the radius of circle: ";
+    cin >> radius;
+
+    // Calculate the circumference of the circle
+    circumference = 2 * pi * radius;
+
+    // Print the result
+    cout << "----------------------------------" << endl;
+    cout << "The circumference of circle is: " << circumference << endl; 
+    cout << "----------------------------------" << endl;
+
+    return 0;
+}
