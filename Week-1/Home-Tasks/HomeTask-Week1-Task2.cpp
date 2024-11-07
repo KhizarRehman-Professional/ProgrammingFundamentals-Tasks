c0028f008a8723da215eef14f012b1a4af3953e3
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-07 16:03:22 +0500

    Create HomeTask-Week1-Task2.cpp

---
+++ Week-1/Home-Tasks/HomeTask-Week1-Task2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+/**
+  Week 1 - Home Task 1 was just installing an IDE for C++ Programming Language. 
+  So, I just skipped it and only uploaded Week 1 - Home Task 2. 
+  
+  @brief Prints the times table for 2 up to 10 using only print statement.
+ */
+int main() {
+    cout << "2  x  1  =  02" << endl;
+    cout << "2  x  2  =  04" << endl;
+    cout << "2  x  3  =  06" << endl;
+    cout << "2  x  4  =  08" << endl;
+    cout << "2  x  5  =  10" << endl;
+    cout << "2  x  6  =  12" << endl;
+    cout << "2  x  7  =  14" << endl;
+    cout << "2  x  8  =  16" << endl;
+    cout << "2  x  9  =  18" << endl;
+    cout << "2  x 10  =  20" << endl;
+    return 0;
+}
