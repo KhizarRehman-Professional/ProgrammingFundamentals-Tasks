adccc81d5d60eae94b69104d4a603a9f8fc83198
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-07 16:17:05 +0500

    Create LabTask-Week1-Task3.cpp

---
+++ Week-1/Lab-Tasks/LabTask-Week1-Task3.cpp
@@ -0,0 +1,16 @@
+// Week 1 - Lab Task 2 was just saving the file and renaming it.
+
+/*
+ * @file LabTask-Week1-Task3.cpp
+ * @author Khizar Rehman
+ * @brief Program to print a welcome message.
+ * @details This program prints a welcome message to the console.
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print welcome message.
+    cout << "Welcome to PF Lab." << endl;
+    return 0;
+}
