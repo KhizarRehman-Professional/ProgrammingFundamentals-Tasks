7d5867ed79933b595280775025cdb81e6103576b
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-07 16:27:01 +0500

    Create LabTask-Week2-Task2.cpp

---
+++ Week-2/Lab-Tasks/LabTask-Week2-Task2.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file    LabTask-Week2-Task2.cpp
+ * @author  Khizar Rehman
+ * @brief   A program that prints a rectangle of asterisks.
+ * @details This program will print a rectangle of asterisks with a height
+ *          and width of 9 asterisks. The asterisks will be placed on the
+ *          border of the rectangle only using the cout operator.
+ */
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "* * * * * * * * * *" << endl;
+    cout << "*                     *" << endl;
+    cout << "*                     *" << endl;
+    cout << "* * * * * * * * * *" << endl;
+
+    return 0;
+}
