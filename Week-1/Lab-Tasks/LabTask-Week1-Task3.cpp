0471a9faeb1df390de65fd0658c38aaacd41e11f
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-07 16:31:01 +0500

    Update LabTask-Week1-Task3.cpp

--- Week-1/Lab-Tasks/LabTask-Week1-Task3.cpp
+++ Week-1/Lab-Tasks/LabTask-Week1-Task3.cpp
@@ -1,9 +1,9 @@
 // Week 1 - Lab Task 2 was just saving the file and renaming it.
 
-/*
- * @file LabTask-Week1-Task3.cpp
- * @author Khizar Rehman
- * @brief Program to print a welcome message.
+/**
+ * @file    LabTask-Week1-Task3.cpp
+ * @author  Khizar Rehman
+ * @brief   Program to print a welcome message.
  * @details This program prints a welcome message to the console.
  */
 #include <iostream>
