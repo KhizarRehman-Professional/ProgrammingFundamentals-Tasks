fd2dd1fb5edad5aa02f96f0cafeaea59871b5d2b
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-17 17:41:58 +0500

    Create 1st-GradedLab-Task4.cpp

---
+++ Week 4/Graded-Lab/1st-GradedLab-Task4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int b;
+    b = sizeof(int);
+    cout << "----------------------------------" << endl;
+    cout << "The Size of Int is: "<< b << " Bytes" << endl;
+    cout << "----------------------------------" << endl;
+    return 0;
+}
