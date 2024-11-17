42951dc3003a4b2b0cde11865a2c8a1a9e5260b1
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-17 17:40:08 +0500

    Create 1st-GradedLab-Task1.cpp

---
+++ Week 4/Graded-Lab/1st-GradedLab-Task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string id = "F2024376070", name = "Khizar Rehman", coursename = "Programming Fundamentals";
+    cout << "----------------------------------" << endl;
+    cout << "Your Id is: "<< id << endl;
+    cout << "Your Name is: "<< name << endl;
+    cout << "Your Course Name is: "<< coursename << endl;
+    cout << "----------------------------------" << endl;
+    return 0;
+}
