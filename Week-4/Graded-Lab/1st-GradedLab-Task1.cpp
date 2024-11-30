01a2417ad2147e8f4e5c0b8be49cfcc65a524901
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-30 19:57:29 +0500

    Update 1st-GradedLab-Task1.cpp

--- Week-4/Graded-Lab/1st-GradedLab-Task1.cpp
+++ Week-4/Graded-Lab/1st-GradedLab-Task1.cpp
@@ -1,14 +1,24 @@
-#include <iostream>
+/**
+ * @file     1st-GradedLab-Task1.cpp
+ * @author   Khizar Rehman
+ * @brief    Program to display student details
+ * @details  Prints student details
+ */
 
+#include <iostream>
 using namespace std;
 
 int main()
 {
+    // Student details
     string id = "F2024376070", name = "Khizar Rehman", coursename = "Programming Fundamentals";
+
+    // Display student information
     cout << "----------------------------------" << endl;
     cout << "Your Id is: "<< id << endl;
     cout << "Your Name is: "<< name << endl;
     cout << "Your Course Name is: "<< coursename << endl;
     cout << "----------------------------------" << endl;
+
     return 0;
 }
