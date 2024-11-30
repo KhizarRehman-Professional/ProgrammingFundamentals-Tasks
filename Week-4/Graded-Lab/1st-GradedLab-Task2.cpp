3fd9789feb825d0569b5fe314fca8ee324f91bc2
KhizarRehman-Professional <khizarrehman.professional@gmail.com>
2024-11-30 19:59:52 +0500

    Create 1st-GradedLab-Task2.cpp

---
+++ Week-4/Graded-Lab/1st-GradedLab-Task2.cpp
@@ -0,0 +1,24 @@
+/*
+ * @file     1st-GradedLab-Task2.cpp
+ * @author   Khizar Rehman
+ * @brief    Program to print a simple box pattern
+ * @details  This program prints a box pattern made of asterisks to the console.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Print the top border of the box
+    cout << "*************" << endl;
+
+    // Print the sides of the box
+    cout << "*   *" << endl;
+    cout << "*   *" << endl;
+
+    // Print the bottom border of the box
+    cout << "*************" << endl;
+
+    return 0;
+}
